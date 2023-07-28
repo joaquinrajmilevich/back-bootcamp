dfcc7004010911ba02ea72c75243f781abd596f1
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2023-07-28 15:49:38 +0200

    fix "" between string data

--- T1/src/Jsonify.java
+++ T1/src/Jsonify.java
@@ -3,7 +3,7 @@ public class Jsonify {
         if (args.length == 0){
             System.out.println("faltan datos");
         }
-        String jsonString = "{\"data\":[%s]}".formatted(String.join("\", \"", args));
+        String jsonString = "{\"data\":[\"%s\"]}".formatted(String.join("\", \"", args));
         System.out.println(jsonString);
     }
 }
